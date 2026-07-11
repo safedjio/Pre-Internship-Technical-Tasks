cd202947d435c28e664097838bc83ba8fb81e2b2
safedjio <6138838@gmail.com>
2026-07-11 14:41:50 +0300

    task 7 : implement EventBus with reflection

---
+++ src/main/java/com/safedjio/internship/task7_eventbus/annotation/Subscribe.java
@@ -0,0 +1,11 @@
+package com.safedjio.internship.task7_eventbus.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Subscribe {
+}
